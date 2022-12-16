276b8dc18b0593f68d7dc14f19aba5560199cbbd
Louise Huyton <louise.huyton@justice.gov.uk>
2022-12-16 15:19:28 +0000

    DTSPO-11424: Add an example test

---
+++ scripts/tests.sh
@@ -0,0 +1,12 @@
+source './../assert.sh'
+
+GLOBAL_DOMAIN="sandbox.platform.hmcts.net"   
+    
+output=$(curl -s "https://plum.$GLOBAL_DOMAIN/health" | jq -r .status)
+
+assert_eq $output "UP"
+if [ "$?" == 0 ]; then
+  log_success "assert_eq returns 0 if two words are equal"
+else
+  log_failure "assert_eq should return 0"
+fi
