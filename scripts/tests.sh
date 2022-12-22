6b114fa7a8185ddfed97b92c86497066a91b0751
Louise Huyton <louise.huyton@justice.gov.uk>
2022-12-22 13:54:05 +0000

    Update tests.sh

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,10 +1,5 @@
-GLOBAL_DOMAIN="sandbox.platform.hmcts.net"   
-    
-output=$(curl -s "https://plum.$GLOBAL_DOMAIN/health" | jq -r .status)
+#!/usr/bin/env bash
 
-assert_eq $output "UP"
-if [ "$?" == 0 ]; then
-  log_success "assert_eq returns 0 if two words are equal"
-else
-  log_failure "assert_eq should return 0"
-fi
+set -e
+    
+curl -v --fail-with-body https://plum.${ENVIRONMENT}.platform.hmcts.net
