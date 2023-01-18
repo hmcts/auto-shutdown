26fbef0f570448ff0c8f16fbd40d5af022859024
Louise Huyton <louise.huyton@justice.gov.uk>
2023-01-18 11:57:29 +0000

    Use Log instead of Echo

--- scripts/aks-auto-start.sh
+++ scripts/aks-auto-start.sh
@@ -14,8 +14,8 @@ jq -c '.[]' <<< $SUBSCRIPTIONS | while read subscription; do
         RESOURCE_GROUP=$(jq -r '.resourceGroup' <<< $cluster)
         NAME=$(jq -r '.name' <<< $cluster)
 
-        echo "About to start cluster $NAME (rg:$RESOURCE_GROUP)"
-        az aks start --resource-group $RESOURCE_GROUP --name $NAME || echo Ignoring any errors starting cluster $NAME 
+        log "About to start cluster $NAME (rg:$RESOURCE_GROUP)"
+        az aks start --resource-group $RESOURCE_GROUP --name $NAME || log Ignoring any errors starting cluster $NAME 
         BUSINESS_AREA=$(jq -r '.tags.businessArea' <<< $cluster)
         if [[ "$BUSINESS_AREA" == "Cross-Cutting" ]]; then
             APP="toffee"
@@ -25,13 +25,13 @@ jq -c '.[]' <<< $SUBSCRIPTIONS | while read subscription; do
 
         ENVIRONMENT=$(jq -r '.tags.environment' <<< $cluster)
 
-        echo "Test that $APP works in $ENVIRONMENT after $NAME start-up"
+        log "Test that $APP works in $ENVIRONMENT after $NAME start-up"
         statuscode=$(curl --max-time 30 --retry 20 --retry-delay 15 -s -o /dev/null -w "%{http_code}"  https://$APP.$ENVIRONMENT.platform.hmcts.net)
 
         if [[ $statuscode -eq 200 ]]; then
-            echo "$APP works in $ENVIRONMENT after $NAME start-up"
+            log "$APP works in $ENVIRONMENT after $NAME start-up"
         else
-            echo "$APP does not work in $ENVIRONMENT after $NAME start-up"
+            log "$APP does not work in $ENVIRONMENT after $NAME start-up"
             curl -X POST --data-urlencode "payload={\"channel\": \"#green-daily-checks\", \"username\": \"AKS Auto-Start\", \"text\": \"$APP does not work in $ENVIRONMENT after $NAME start-up. Please check cluster.\", \"icon_emoji\": \":tim-webster:\"}" \
             ${registrySlackWebhook} 
         fi
