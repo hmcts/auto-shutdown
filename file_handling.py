15587ba6523cf8c5fad1600facb8460d82427a1d
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-06-23 07:12:27 +0100

    initial

---
+++ file_handling.py
@@ -0,0 +1,29 @@
+
+import json
+import os
+from datetime import datetime
+from datetime import date
+
+listObj = []
+filepath = 'issues_list.json'
+new_data = os.environ.get('NEW_DATA')
+today = date.today()
+print(new_data)
+try:
+  with open(filepath, "r") as json_file:
+    listObj = json.load(json_file)
+except FileNotFoundError:
+  with open(filepath, "w") as json_file:
+    listObj.append(new_data)
+    json.dump(listObj, json_file, indent=4)
+else:
+  for x in range(len(listObj)):
+    d = json.loads(listObj[x]) 
+    end_date = datetime.strptime(d['Change_End_Date'], '%d/%m/%Y').date()
+    if today > end_date:
+         listObj.pop(x)
+  if new_data:
+    listObj.append(new_data)
+  with open(filepath, "w") as json_file:
+    json.dump(listObj, json_file, indent=4)
+
