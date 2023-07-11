366f0c8e6842d3d09f85b7760ddc05f8f6c70a38
Jordan Hoey <jordan.hoey@justice.gov.uk>
2023-07-11 16:30:50 +0100

    expanding var names for readability

--- scripts/future-auto-shutdown.sh
+++ scripts/future-auto-shutdown.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #set -x
 shopt -s nocasematch
-RED='\033[0;31m'
+Rend_date='\033[0;31m'
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 
@@ -10,58 +10,58 @@ jq -c '.[]' <<< $SUBSCRIPTIONS | while read subscription; do
     SUBSCRIPTION_ID=$(jq -r '.id' <<< $subscription)
     az account set -s $SUBSCRIPTION_ID
     #CLUSTERS=$(az resource list \
-    #--resource-type Microsoft.ContainerService/managedClusters \
+    #--resource-type Microsoft.ContainerService/managend_dateClusters \
     #--query "[?tags.autoShutdown == 'true']" -o json)
     CLUSTERS=$(az resource list \
-        --resource-type Microsoft.ContainerService/managedClusters -o json)
+        --resource-type Microsoft.ContainerService/managend_dateClusters -o json)
 
     jq -c '.[]' <<< $CLUSTERS | while read cluster; do
         RESOURCE_GROUP=$(jq -r '.resourceGroup' <<< $cluster)
         SKIP="false"
-        NAME=$(jq -r '.name' <<< $cluster)
+        cluster_name=$(jq -r '.name' <<< $cluster)
         echo "---------------------"
-        ENV=$(echo $NAME|cut -d'-' -f2)
-        ENV=${ENV/#sbox/Sandbox}
-        ENV=${ENV/stg/Staging}
-        BU=$(echo $NAME|cut -d'-' -f1)
-        BU=${BU/ss/cross-cutting}
-        echo $NAME $BU $ENV
+        cluster_env=$(echo $cluster_name|cut -d'-' -f2)
+        cluster_env=${cluster_env/#sbox/Sandbox}
+        cluster_env=${cluster_env/stg/Staging}
+        cluster_business_area=$(echo $cluster_name|cut -d'-' -f1)
+        cluster_business_area=${cluster_business_area/ss/cross-cutting}
+        echo $cluster_name $cluster_business_area $cluster_env
         while read id
         do
-            BA=$(jq -r '."business_area"' <<< $id)
-            ENVT=$(jq -r '."environment"' <<< $id)
-            SD=$(jq -r '."skip_start_date"' <<< $id)
-            ED=$(jq -r '."skip_end_date"' <<< $id)
+            business_area_entry=$(jq -r '."business_area"' <<< $id)
+            env_entry=$(jq -r '."env_entry"' <<< $id)
+            start_date=$(jq -r '."skip_start_date"' <<< $id)
+            end_date=$(jq -r '."skip_end_date"' <<< $id)
             #start date formatting
-            SDF=$(awk -F'-' '{printf("%04d-%02d-%02d\n",$3,$2,$1)}' <<< $SD)
-            SDS=$(date -d "$SDF 00:00:00" +%s)
+            start_date_formatting=$(awk -F'-' '{printf("%04d-%02d-%02d\n",$3,$2,$1)}' <<< $start_date)
+            start_date_seconds=$(date -d "$start_date_formatting 00:00:00" +%s)
             #end date formatting
-            EDF=$(awk -F'-' '{printf("%04d-%02d-%02d\n",$3,$2,$1)}' <<< $ED)
-            EDS=$(date -d "$EDF 00:00:00" +%s)
+            end_date_formatting=$(awk -F'-' '{printf("%04d-%02d-%02d\n",$3,$2,$1)}' <<< $end_date)
+            end_date_seconds=$(date -d "$end_date_formatting 00:00:00" +%s)
             #current date formatting
             current_date=$(date +'%d-%m-%Y')
-            CDF=$(awk -F'-' '{printf("%04d-%02d-%02d\n",$3,$2,$1)}' <<< $current_date)
-            CDS=$(date -d "$CDF 00:00:00" +%s)
+            current_date_formatting=$(awk -F'-' '{printf("%04d-%02d-%02d\n",$3,$2,$1)}' <<< $current_date)
+            current_date_seconds=$(date -d "$current_date_formatting 00:00:00" +%s)
             #Skip logic
-            if [[ ${ENVT} =~ ${ENV} ]] && [[ $BU == $BA ]] && [[ $SDS -eq $CDS ]] ; then
+            if [[ ${env_entry} =~ ${cluster_env} ]] && [[ $cluster_business_area == $business_area_entry ]] && [[ $start_date_seconds -eq $current_date_seconds ]] ; then
                 echo "Match: $id"
                 SKIP="true"
                 continue
 
-            elif [[ ${ENVT} =~ ${ENV} ]] && [[ $BU == $BA ]] && [[ $CDS -ge $SDS ]] &&[[ $CDS -le $EDS ]]; then
+            elif [[ ${env_entry} =~ ${cluster_env} ]] && [[ $cluster_business_area == $business_area_entry ]] && [[ $current_date_seconds -ge $start_date_seconds ]] &&[[ $current_date_seconds -le $end_date_seconds ]]; then
                 echo "Match : $id"
                 SKIP="true"
                 continue
             fi
         done < <(jq -c '.[]' issues_list.json)
         if [[ $SKIP == "false" ]]; then
-            echo -e "${GREEN}About to shutdown cluster $NAME (rg:$RESOURCE_GROUP)"
+            echo -e "${GREEN}About to shutdown cluster $cluster_name (rg:$RESOURCE_GROUP)"
         else
-            echo -e "${YELLOW}cluster $NAME (rg:$RESOURCE_GROUP) has been skipped from todays shutdown schedule"
+            echo -e "${YELLOW}cluster $cluster_name (rg:$RESOURCE_GROUP) has been skippend_date from todays shutdown schend_dateule"
         fi
 
 
-    #echo az aks stop --resource-group $RESOURCE_GROUP --name $NAME || echo Ignoring any errors stopping cluster
-    #-az aks stop --resource-group $RESOURCE_GROUP --name $NAME || echo Ignoring any errors stopping cluster
+    #echo az aks stop --resource-group $RESOURCE_GROUP --name $cluster_name || echo Ignoring any errors stopping cluster
+    #-az aks stop --resource-group $RESOURCE_GROUP --name $cluster_name || echo Ignoring any errors stopping cluster
     done # end_of_cluster_loop
 done
