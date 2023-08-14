241a756db18b2fa108b96a8be3b9371654065da5
Jordan Hoey <jordan.hoey@justice.gov.uk>
2023-08-14 14:11:18 +0100

    updating script

--- cost-calculator.py
+++ cost-calculator.py
@@ -49,10 +49,12 @@ def calculate_cost(env_rate, vm_num_int, skip_bus_days, skip_weekend_days):
     total_hours = (bus_hours + weekend_hours)
     vm_cost = (env_rate * total_hours)*vm_num_int
     total_cost = ((vm_cost // 100) * 25) + vm_cost
-    cost_output = str(round(total_cost, 2))
-
+    cost_output = round(total_cost, 2)
+    cost_output_formatted = f"{cost_output:,}"
+    
     with open(env_file_path, 'a') as env_file:
-        env_file.write('\n' + "COST_DETAILS=" + cost_output)
+        env_file.write('\n' + "COST_DETAILS=" + str(cost_output) + '\n')
+        env_file.write("COST_DETAILS_FORMATTED=" + str(cost_output_formatted))
         env_file.close()
 
 calculate_cost(vm_hour_rate, vm_num_int, business_days, weekend_days)
