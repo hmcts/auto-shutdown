820383a3073861762774c505ba9142c2a6ca1fae
mokainos <72137340+mokainos@users.noreply.github.com>
2023-08-25 09:19:41 +0100

    Update auto-start.sh

--- scripts/flexible-server/auto-start.sh
+++ scripts/flexible-server/auto-start.sh
@@ -8,10 +8,11 @@ do
     jq -c '.[]'<<< $SERVERS | while read server
     do
         ID=$(jq -r '.id' <<< $server)
+        NAME=$(jq -r '.name' <<< $server)
         status=$(az postgres flexible-server show  --ids $ID --query "state")
         if [[  "$status" != *"Ready"* ]]; then
-            echo "Starting flexible-server show  in Subscription: $(az account show --query name)  ResourceGroup: $(jq -r '.resourceGroup' <<< $server)  Name: $(jq -r '.name' <<< $server)"
-            az postgres flexible-server start --ids $ID --no-wait || echo Ignoring errors starting flexible server
+            echo "Starting flexible-server show  in Subscription: $(az account show --query name)  ResourceGroup: $(jq -r '.resourceGroup' <<< $server)  Name: $NAME"
+            az postgres flexible-server start --ids $ID --no-wait || echo Ignoring error starting $NAME
         fi
     done
 done   
