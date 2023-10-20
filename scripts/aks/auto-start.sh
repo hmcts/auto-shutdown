f76cbac9eb7adfc22c3964a1549d8e9d11d3f54d
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-10-20 10:35:55 +0100

    fix

--- scripts/aks/auto-start.sh
+++ scripts/aks/auto-start.sh
@@ -79,23 +79,15 @@ function process_clusters() {
     echo "Waiting 10 mins to give clusters time to start before testing pods"
     sleep 600
 
-    jq -c '.[]' <<< $SUBSCRIPTIONS | while read subscription; do
+     jq -c '.[]' <<< $SUBSCRIPTIONS | while read subscription; do
         subscription
         jq -c '.[]' <<< $CLUSTERS | while read cluster; do
             cluster
             check_cluster_status
+            POWER_STATE=$(az aks show --name  $NAME -g $RESOURCE_GROUP | jq -r .powerState.code)
+            ts_echo "cluster: $NAME, Power State : ${RESULT}"
         done
-    done
-
-    jq -c '.[]' <<< $SUBSCRIPTIONS | while read subscription; do
-        subscription
-        jq -c '.[]' <<< $CLUSTERS | while read cluster; do
-            cluster
-            ts_echo $NAME
-            RESULT=$(az aks show --name  $NAME -g $RESOURCE_GROUP | jq -r .powerState.code)
-            ts_echo "${RESULT}"
-        done
-    done
+      done
 }
 
 SUBSCRIPTIONS=$(az account list -o json)
