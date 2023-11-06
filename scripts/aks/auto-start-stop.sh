152db7eda597460fdb86f9a15af5d944875e6979
Vik <viktoriya.bogdanova@hmcts.net>
2023-11-06 12:39:20 +0000

    revert PR 202

--- scripts/aks/auto-start-stop.sh
+++ scripts/aks/auto-start-stop.sh
@@ -5,7 +5,7 @@ AMBER='\033[1;33m'
 GREEN='\033[0;32m'
 source scripts/aks/common-functions.sh
 
-MODE=${1:-stop}
+MODE=${1:-start}
 SKIP="false"
 
 if [[ "$MODE" != "start" && "$MODE" != "stop" ]]; then
@@ -28,9 +28,9 @@ jq -c '.[]' <<< $SUBSCRIPTIONS | while read subscription; do
 
     if [[ $SKIP == "false" ]]; then
       echo -e "${GREEN}About to run $MODE operation on cluster $CLUSTER_NAME (rg:$RESOURCE_GROUP)"
+      echo az aks $MODE --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --no-wait || echo Ignoring any errors while $MODE operation on cluster
       if [[ $DRYRUN != "true" ]]; then
-        echo az aks $MODE --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --no-wait || echo Ignoring any errors while $MODE operation on cluster
-        # az aks $MODE --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --no-wait || echo Ignoring any errors while $MODE operation on cluster
+        az aks $MODE --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --no-wait || echo Ignoring any errors while $MODE operation on cluster
       fi
     else
       echo -e "${AMBER}cluster $CLUSTER_NAME (rg:$RESOURCE_GROUP) has been skipped from today's $MODE operation schedule"
