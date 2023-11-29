a5cae334cef02b2b9e140b30cd5e30b0673bc7f9
Vik <viktoriya.bogdanova@hmcts.net>
2023-11-29 10:17:06 +0000

    refactor vm start stop

--- scripts/vm/auto-start-stop.sh
+++ scripts/vm/auto-start-stop.sh
@@ -16,18 +16,33 @@ if [[ "$MODE" != "start" && "$MODE" != "stop" ]]; then
 fi
 
 SUBSCRIPTIONS=$(az account list -o json)
+SORTED_SUBSCRIPTIONS=$(jq -r '[
+    ( .[] | select(.name != "HMCTS-HUB-NONPROD-INTSVC" )),
+    ( .[] | select(.name == "HMCTS-HUB-NONPROD-INTSVC" ))
+]' <<<$SUBSCRIPTIONS)
+IS_HUB_NEEDED="false"
 
-jq -c '.[]' <<<$SUBSCRIPTIONS | while read subscription; do
+for row in $(echo "$SORTED_SUBSCRIPTIONS" | jq -r '.[] | @base64' ); do
+    _jq() {
+    echo ${row} | base64 --decode | jq -r ${1}
+    }
+
+    SUBSCRIPTION_ID=$(_jq '.id')
+    SUBSCRIPTION_NAME=$(_jq '.name')
 	get_subscription_vms
-    if [[ $SUBSCRIPTION_NAME == "HMCTS-HUB-NONPROD-INTSVC" ]]; then
-        continue
-    fi
-	jq -c '.[]' <<<$VMS | while read vm; do
-        get_vm_details
-		ID=$(jq -r '.id' <<<$vm)
-        name=$(jq -r '.name' <<<$vm)
-        rg=$(jq -r '.resourceGroup' <<<$vm)
-        ENVIRONMENT=$(jq -r '.tags.environment' <<<$vm)
+    if [[ $SUBSCRIPTION_NAME == "HMCTS-HUB-NONPROD-INTSVC" && $IS_HUB_NEEDED == "true" && $MODE == "stop" ]]; then
+		continue
+	fi
+    for vm_row in $(echo "$VMS" | jq -r '.[] | @base64' ); do
+        _jq_vm() {
+        echo ${vm_row} | base64 --decode | jq -r ${1}
+        }
+        RESOURCE_GROUP=$(_jq_vm '.resourceGroup')
+        VM_NAME=$(_jq_vm '.name')
+        STARTUP_MODE=$(_jq_vm '.tags.startupMode')
+		ID=$(_jq_vm '.id')
+        ENVIRONMENT=$(_jq_vm '.tags.environment')
+        VM_BUSINESS_AREA=$(_jq_vm '.tags.businessArea')
         declare -A vm_envs=(
             [sandbox]="sbox"
             [testing]="test"
@@ -42,17 +57,15 @@ jq -c '.[]' <<<$SUBSCRIPTIONS | while read subscription; do
         else
             ENV_SUFFIX="$ENVIRONMENT"
         fi
-        vm_business_area=$(jq -r '.tags.businessArea' <<<$vm)
 		status=$(az vm show -d --ids $ID --query "powerState")
-        SKIP=$(should_skip_start_stop $ENV_SUFFIX $vm_business_area $MODE) 
-
+        SKIP=$(should_skip_start_stop $ENV_SUFFIX $VM_BUSINESS_AREA $MODE) 
 		if [[ $SKIP == "false" ]]; then
             echo -e "${GREEN}About to run $MODE operation on vm $VM_NAME (rg:$RESOURCE_GROUP)"
             echo az vm $MODE --ids $ID --no-wait || echo Ignoring any errors while $MODE operation on vm
             az vm $MODE --ids $ID --no-wait || echo Ignoring any errors while $MODE operation on vm
         else
             echo -e "${AMBER}vm $VM_NAME (rg:$RESOURCE_GROUP) has been skipped from today's $MODE operation schedule"
+            IS_HUB_NEEDED="true"
         fi
-        
 	done
 done
--- scripts/vm/common-functions.sh
+++ scripts/vm/common-functions.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 function get_subscription_vms() {
-  SUBSCRIPTION_ID=$(jq -r '.id' <<< $subscription)
-  SUBSCRIPTION_NAME=$(jq -r '.name' <<<$subscription)
   az account set -s $SUBSCRIPTION_ID
   VMS=$(az resource list --resource-type Microsoft.Compute/virtualMachines --query "[?tags.autoShutdown == 'true']" -o json)
 }
