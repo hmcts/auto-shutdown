82c95cb68de75f6576e206defef8ce22c5a31290
Vik <viktoriya.bogdanova@hmcts.net>
2023-11-29 10:22:26 +0000

    refactor vm start stop

--- scripts/vm/common-functions.sh
+++ scripts/vm/common-functions.sh
@@ -4,9 +4,3 @@ function get_subscription_vms() {
   az account set -s $SUBSCRIPTION_ID
   VMS=$(az resource list --resource-type Microsoft.Compute/virtualMachines --query "[?tags.autoShutdown == 'true']" -o json)
 }
-
-function get_vm_details() {
-  RESOURCE_GROUP=$(jq -r '.resourceGroup' <<< $vm)
-  VM_NAME=$(jq -r '.name' <<< $vm)
-  STARTUP_MODE=$(jq -r '.tags.startupMode' <<< $vm)
-}
