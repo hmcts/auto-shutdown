6faba02153212fc9e132190da0f961db95f55775
Marty Fox <martin.fox1@justice.gov.uk>
2024-07-24 10:53:40 +0100

    weird indentation, not showing locally

--- scripts/vm/vmstatus.sh
+++ scripts/vm/vmstatus.sh
@@ -45,7 +45,7 @@ do
         #    - If MODE = Start then a stopped App Gateway is incorrect and we should notify
         #    - If MODE = Stop then a running App Gateway is incorrect and we should notify
         #    - If neither Running or Stopped is found then something else is going on and we should notify
-        if [[ "$VM_STATE" =~ .*"running".* ]]; then
+		if [[ "$VM_STATE" =~ .*"running".* ]]; then
 			ts_echo_color $( [[ $MODE == "start" ]] && echo GREEN || echo RED ) "$logMessage"
 			if [[ $MODE == "stop" ]]; then
 				auto_shutdown_notification ":red_circle: $slackMessage"
