dc607f60a45a51ac48b6584a6674cbfd13537134
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-09-16 15:36:50 +0100

    adding some debug logs for initial testing

--- scripts/flexible-server/common-functions.sh
+++ scripts/flexible-server/common-functions.sh
@@ -3,14 +3,21 @@
 # Function that accepts the flexible sql server json as input and sets variables for later use to stop or start the flexible sql server
 function get_flexible_sql_server_details() {
   RESOURCE_GROUP=$(jq -r '.resourceGroup' <<< $flexibleserver)
+  log "$RESOURCE_GROUP"
   SERVER_ID=$(jq -r '.id' <<< $flexibleserver)
+  log "$SERVER_ID"
   SERVER_NAME=$(jq -r '.name' <<< $flexibleserver)
+  log "$SERVER_NAME"
   ENVIRONMENT=$(echo $SERVER_NAME | rev | cut -d'-' -f 1 | rev )
+  log "$ENVIRONMENT"
   BUSINESS_AREA=$( jq -r 'if (.tags.businessArea | ascii_downcase) == "ss" then "cross-cutting" else .tags.businessArea | ascii_downcase end' <<< $flexibleserver)
+  log "$BUSINESS_AREA"
   STARTUP_MODE=$(jq -r '.tags.startupMode' <<< $flexibleserver)
+  log "$STARTUP_MODE"
   SERVER_STATE=$(jq -r '.properties_state' <<< $flexibleserver)
+  log "$SERVER_STATE"
   SUBSCRIPTION=$(jq -r '.subscriptionId' <<< $flexibleserver)
-
+  log "$SUBSCRIPTION"
 }
 
 function flexible_server_state_messages() {
