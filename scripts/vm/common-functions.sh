4368551e6a85012319756a2c590f3d3726aaf5eb
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-10-28 13:55:34 +0000

    updating VM script to work with ms graph

--- scripts/vm/common-functions.sh
+++ scripts/vm/common-functions.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 
-# Function that uses the subscription input to get set variables for later use and gather all VMs within the subscription for shutdown
-function get_subscription_vms() {
-  SUBSCRIPTION_ID=$(jq -r '.id' <<< $subscription)
-  SUBSCRIPTION_NAME=$(jq -r '.name' <<< $subscription)
-  az account set -s $SUBSCRIPTION_ID
-  VMS=$(az resource list --resource-type Microsoft.Compute/virtualMachines --query "[?tags.autoShutdown == 'true']" -o json)
+function get_vms() {
+    #MS az graph query to find and return a list of all AKS tagged to be included in the auto-shutdown process.
+    log "----------------------------------------------"
+    log "Running az graph query..."
+
+    if [ -z $1 ]; then
+        env_selector=""
+    elif [ $1 == "untagged" ]; then
+        env_selector="| where isnull(tags.environment) and isnull(tags.Environment)"
+    else
+        env_selector="| where tags.environment contains '$1' or tags.Environment contains '$1'"
+    fi
+
+    if [ -z $2 ]; then
+        area_selector=""
+    else
+        area_selector="| where tags.businessArea == '$2'"
+    fi
+
+    az graph query -q "
+    resources
+    | where type =~ 'Microsoft.Compute/virtualMachines'
+    | where tags.autoShutdown == 'true'
+    $env_selector
+    $area_selector
+    | project name, resourceGroup, subscriptionId, ['tags'], properties, ['id']
+    " --first 1000 -o json
+
+    log "az graph query complete"
 }
 
 # Function that accepts the VM json as input and sets variables for later use to stop or start VM
 function get_vm_details() {
-  RESOURCE_GROUP=$(jq -r '.resourceGroup' <<< $vm)
-  VM_ID=$(jq -r '.id' <<< $vm)
+  RESOURCE_GROUP=$(jq -r '.resourceGroup // "value_not_retrieved"' <<< $vm)
+  VM_ID=$(jq -r '.properties.vmId' <<< $vm)
   VM_NAME=$(jq -r '.name' <<< $vm)
-  ENVIRONMENT=$(jq -r '.tags.environment' <<< $vm)
-  BUSINESS_AREA=$( jq -r 'if (.tags.businessArea | ascii_downcase?) == "ss" then "cross-cutting" else .tags.businessArea | ascii_downcase? end' <<< $vm)
+  ENVIRONMENT=$(jq -r '.tags.environment // .tags.Environment // "tag_not_set"' <<< "$vm")
+  BUSINESS_AREA=$(jq -r 'if (.tags.businessArea // .tags.BusinessArea // "tag_not_set" | ascii_downcase) == "ss" then "cross-cutting" else (.tags.businessArea // .tags.BusinessArea // "tag_not_set" | ascii_downcase) end' <<< $vm)
   STARTUP_MODE=$(jq -r '.tags.startupMode // "false"' <<< $vm)
-  VM_STATE=$(az vm show -d --ids $VM_ID --query "powerState" | jq -r)
-
+  VM_STATE=$(jq -r '.properties.extended.instanceView.powerState.code' <<< $vm)
+  SUBSCRIPTION=$(jq -r '.subscriptionId' <<<$vm)
 }
 
 function vm_state_messages() {
--- scripts/vm/vmstatus.sh
+++ scripts/vm/vmstatus.sh
@@ -18,51 +18,34 @@ if [[ "$MODE" != "start" && "$MODE" != "deallocate" ]]; then
 	exit 1
 fi
 
-# Find all subscriptions that are available to the credential used and saved to SUBSCRIPTIONS variable
-SUBSCRIPTIONS=$(az account list -o json)
+VMS=$(get_vms)
 
-# For each subscription found, start the loop
-jq -c '.[]' <<< $SUBSCRIPTIONS | while read subscription
-do
+# For each VM found in the function `get_vms` start another loop
+jq -c '.data[]' <<<$VMS | while read vm; do
 
-	# Function that returns the Subscription Id and Name as variables,
-	# sets the subscription as the default then returns a json formatted variable of available VMs with an autoshutdown tag
-	get_subscription_vms
-	echo "Scanning $SUBSCRIPTION_NAME..."
+    # Function that returns the Resource Group, Id and Name of the VMs and its current state as variables
+    get_vm_details
 
-	# For each App Gateway found in the function `get_subscription_vms` start another loop
-	jq -c '.[]' <<<$VMS | while read vm
-	do
+    log "====================================================="
+    log "Processing Virtual Machine: $VM_NAME in Resource Group: $RESOURCE_GROUP"
+    log "====================================================="
 
-		# Function that returns the Resource Group, Id and Name of the VMs and its current state as variables
-		get_vm_details
+    if [[ $ENVIRONMENT == "development" ]]; then
+        VM_ENV=${ENVIRONMENT/development/'Preview / Dev'}
+    elif [[ $ENVIRONMENT == "testing" ]]; then
+        VM_ENV=${ENVIRONMENT/testing/'Test / Perftest'}
+    else;
+        VM_ENV=$(to_lowercase "$ENVIRONMENT")
+    fi
 
-        # Declare and populate a map of environments and real names
-        declare -A vm_envs=(
-            [sandbox]="sbox"
-            [testing]="test"
-            [staging]="aat"
-            [development]="dev"
-            [production]="prod"
-            [demo]="demo"
-            [ithc]="ithc"
-        )
-
-        # Check the map of environments using the ENVIRONMENT returned by `get_vm_details` to see if one is found
-        # If found set the name based on the value from the `vm_envs` variable map
-        if [[ "${vm_envs[$ENVIRONMENT]}" ]]; then
-            ENV_SUFFIX="${vm_envs[$ENVIRONMENT]}"
-        else
-            ENV_SUFFIX="$ENVIRONMENT"
-        fi
-
-        # SKIP variable updated based on the output of the `should_skip_start_stop` function which calculates its value based
-        # on a tag named `startupMode` and the `issues_list.json` file which contains user requests to keep environments online after normal hours
-        SKIP=$(should_skip_start_stop $ENV_SUFFIX $BUSINESS_AREA $MODE)
+    # SKIP variable updated based on the output of the `should_skip_start_stop` function which calculates its value based
+    # on a tag named `startupMode` and the `issues_list.json` file which contains user requests to keep environments online after normal hours
+    log "checking skip logic for env: $VM_ENV, business_area: $BUSINESS_AREA, mode: $MODE"
+    SKIP=$(should_skip_start_stop $VM_ENV $BUSINESS_AREA $MODE)
 
         # Setup message output templates for later use
 		logMessage="VM: $VM_NAME in ResourceGroup: $RESOURCE_GROUP is $VM_STATE state after $MODE action."
-		slackMessage="VM: *$VM_NAME* in Subscription: *$SUBSCRIPTION_NAME*  ResourceGroup: *$RESOURCE_GROUP* is *$VM_STATE* state after *$MODE* action."
+		slackMessage="VM: *$VM_NAME* in Subscription: *$SUBSCRIPTION*  ResourceGroup: *$RESOURCE_GROUP* is *$VM_STATE* state after *$MODE* action."
 
         # If SKIP is false then we progress with the status chec for the particular VM in this loop run, if SKIP is true then do nothing
         if [[ $SKIP == "false" ]]; then
@@ -72,14 +55,14 @@ do
 		#    - If MODE = deallocate then a running VM is incorrect and we should notify
 		#    - If neither Running or Stopped is found then something else is going on and we should notify
             case "$VM_STATE" in
-                *"running"*)
+                *"PowerState/running"*)
                     ts_echo_color $( [[ $MODE == "start" ]] && echo GREEN || echo RED ) "$logMessage"
                     if [[ $MODE == "deallocate" ]]; then
                         auto_shutdown_notification ":red_circle: $slackMessage"
                         add_to_json "$VM_ID" "$VM_NAME" "$slackMessage" "vm" "$MODE"
                     fi
                     ;;
-                *"deallocated"*)
+                *"PowerState/deallocated"*)
                     ts_echo_color $( [[ $MODE == "start" ]] && echo RED || echo GREEN ) "$logMessage"
                     if [[ $MODE == "start" ]]; then
                         auto_shutdown_notification ":red_circle: $slackMessage"
@@ -95,5 +78,4 @@ do
         else
             ts_echo_color AMBER "VM: $VM_NAME in Resource Group: $RESOURCE_GROUP has been skipped from today's $MODE operation schedule"
         fi
-	done
 done
