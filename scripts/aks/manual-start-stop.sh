6f128ec382beb161b14a49c6e0f2f060772e2e9c
Jordan Hoey <125922012+JordanHoey96@users.noreply.github.com>
2024-10-28 17:20:21 +0000

    Revert "Development update"

--- scripts/aks/manual-start-stop.sh
+++ scripts/aks/manual-start-stop.sh
@@ -62,9 +62,7 @@ fi
 jq -c '.data[]' <<< "$CLUSTERS" | while read -r cluster; do
     get_cluster_details  # Assuming this function processes individual clusters
 
-    log "================================================================================"
-    log "Processing Cluster: $CLUSTER_NAME, RG: $RESOURCE_GROUP, SUB: $SUBSCRIPTION"
-    log "================================================================================"
+    ts_echo_color BLUE "Processing Cluster: $CLUSTER_NAME, RG: $RESOURCE_GROUP, SUB: $SUBSCRIPTION"
 
     if [[ "$DEV_ENV" != "true" ]]; then
         aks_state_messages  # Function for displaying state messages
