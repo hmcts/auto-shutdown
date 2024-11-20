ff97628a1366187949f3d6def6ab0ab37753a5e4
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-11-20 15:10:03 +0000

    removing text echo

--- scripts/aks/send-slack-message.sh
+++ scripts/aks/send-slack-message.sh
@@ -46,7 +46,4 @@ jq --arg issue_url "$request_url_link" \
 
 MESSAGE=$(< slack-payload.json)
 
-echo "====Testing slack message===="
-echo "$MESSAGE"
-
 curl -X POST -H 'Content-type: application/json' --data "${MESSAGE}" ${SLACK_WEBHOOK}
