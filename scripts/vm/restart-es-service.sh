2a8911ca87cfc195d13fa4373a9c35ff05f14fcb
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-12-11 15:54:50 +0000

    Update if condition

--- scripts/vm/restart-es-service.sh
+++ scripts/vm/restart-es-service.sh
@@ -42,12 +42,14 @@ esac
 for REMOTE_HOST in "${REMOTE_HOSTS[@]}"; do
   STATUS=$(ssh -o ConnectTimeout=20 -o StrictHostKeyChecking=no -i "${PRIVATE_KEY}" ${REMOTE_USER}@${REMOTE_HOST} "${CHECK_COMMAND}")
 	echo $STATUS
-  if [[ "${STATUS}" == "active" ]]; then
-		echo "Elasticsearch service on ${REMOTE_HOST} is active."
-	elif [[ -z "${STATUS}" ]]; then
+  if [[ -z "${STATUS}" ]]; then
 		echo "Problem connecting to ${REMOTE_HOST}."
-	else
-		echo "Restarting Elasticsearch service on ${REMOTE_HOST}"
-    ssh -o ConnectTimeout=20 -o StrictHostKeyChecking=no -i "${PRIVATE_KEY}" ${REMOTE_USER}@${REMOTE_HOST} "${RESTART_COMMAND}"
-  fi
+	else 
+		if [[ "${STATUS}" == "active" ]]; then
+			echo "Elasticsearch service on ${REMOTE_HOST} is active."
+		else
+			echo "Restarting Elasticsearch service on ${REMOTE_HOST}"
+    	ssh -o ConnectTimeout=20 -o StrictHostKeyChecking=no -i "${PRIVATE_KEY}" ${REMOTE_USER}@${REMOTE_HOST} "${RESTART_COMMAND}"
+		fi
+	fi
 done
