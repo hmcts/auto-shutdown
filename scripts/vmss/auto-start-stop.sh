a27f6c7b3ef683d6b62aabb1251c1dcaf339ad0a
Narinder Doal <nsd17@hotmail.co.uk>
2025-02-20 15:28:56 +0000

    add vmss retrieval code

--- scripts/vmss/auto-start-stop.sh
+++ scripts/vmss/auto-start-stop.sh
@@ -3,5 +3,20 @@
 shopt -s nocasematch
 
 # Source shared function scripts
-# source scripts/vmss/common-functions.sh
-# source scripts/common/common-functions.sh
+source scripts/vmss/common-functions.sh
+source scripts/common/common-functions.sh
+
+# Set variables for later use, MODE has a default but can be overridden at usage time
+MODE=${1:-start}
+SKIP="false"
+
+# Catch problems with MODE input, must be one of Start/Stop
+if [[ "$MODE" != "start" && "$MODE" != "deallocate" ]]; then
+    echo "Invalid MODE. Please use 'start' or 'deallocate'."
+    exit 1
+fi
+
+VMS=$(get_vmss "$2")
+vm_count=$(jq -c -r '.count' <<<$VMS)
+log "$vmss_count VMSS's found"
+log "----------------------------------------------"
