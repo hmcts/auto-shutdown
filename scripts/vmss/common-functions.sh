e6e39940189508702bb4c354a774fc1477204b13
Narinder Doal <nsd17@hotmail.co.uk>
2025-02-21 11:09:52 +0000

    add common fucntions code for vmss

--- scripts/vmss/common-functions.sh
+++ scripts/vmss/common-functions.sh
@@ -1,1 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Script that allows users to manually start environments via GitHub workflows and accepts inputs from the workflow
+
+# set -x
+shopt -s nocasematch
+
+# Source shared function scripts
+source scripts/vmss/common-functions.sh
+source scripts/common/common-functions.sh
+
+# Check and set default MODE if not provided
+MODE=${1:-start}
+
+# Check if MODE is stop and set to deallocate
+if [[ $MODE == "stop" ]]; then
+    MODE="deallocate"
+fi
+
