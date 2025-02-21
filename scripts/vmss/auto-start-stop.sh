614040ebcd972cabebe7e82e7c0010f1c7a920c7
Narinder Doal <nsd17@hotmail.co.uk>
2025-02-21 11:00:18 +0000

    start and skip logic

--- scripts/vmss/auto-start-stop.sh
+++ scripts/vmss/auto-start-stop.sh
@@ -37,3 +37,23 @@ elif [[ $ENVIRONMENT == "testing" ]]; then
 else
     VM_ENV=$(to_lowercase "$ENVIRONMENT")
 fi
+
+
+# SKIP variable updated based on the output of the `should_skip_start_stop` function which calculates its value based
+# on a tag named `startupMode` and the `issues_list.json` file which contains user requests to keep environments online after normal hours
+log "checking skip logic for env: $VM_ENV, business_area: $BUSINESS_AREA, mode: $MODE"
+SKIP=$(should_skip_start_stop $VM_ENV $BUSINESS_AREA $MODE)
+log "SKIP evaluated to $SKIP"
+
+# If SKIP is false then we progress with the action (deallocate/start) for the particular VMSS instance, otherwise, skip and log
+if [[ $SKIP == "false" ]]; then
+    if [[ $DEV_ENV != "true" ]]; then
+        vm_state_messages
+        az vmss $MODE --instance-ids $VM_INSTANCE_ID --resource-group $RESOURCE_GROUP --name $VMSS_NAME --no-wait || echo "Ignoring any errors while performing $MODE operation on VMSS instance"
+    else
+        ts_echo_color BLUE "Development Env: simulating state commands only."
+        vm_state_messages
+    fi
+else
+    ts_echo_color AMBER "VMSS Instance: $VM_INSTANCE_ID in Scale Set: $VMSS_NAME (Resource Group: $RESOURCE_GROUP) has been skipped from today's $MODE operation schedule"
+fi
