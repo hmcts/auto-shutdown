6d7dd2a2359f8a5f436da4bfe63860299415aa7e
Narinder Doal <nsd17@hotmail.co.uk>
2025-03-20 10:55:37 +0000

    add dcd-cnp-qa & dcd-cnp-dev to vmss

--- scripts/vmss/common-functions.sh
+++ scripts/vmss/common-functions.sh
@@ -32,7 +32,7 @@ function get_vmss() {
    az graph query -q "
     computeresources
     | where type =~ 'Microsoft.Compute/virtualMachineScaleSets/VirtualMachines'
-    | where subscriptionId in ('bf308a5c-0624-4334-8ff8-8dca9fd43783')
+    | where subscriptionId in ('bf308a5c-0624-4334-8ff8-8dca9fd43783','7a4e3bd5-ae3a-4d0c-b441-2188fee3ff1c', '1c4f0704-a29e-403d-b719-b90c34ef14c9')
     | where tags.autoShutdown == 'true'
     | where not (name matches regex '(^aks-|-aks-|-aks$)')
     $env_selector
