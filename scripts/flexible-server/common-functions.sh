a6c68e7ab85a860b3657c538a1fe8072a3972a7e
Asaleem-hmcts <Aamir.Saleem@HMCTS.NET>
2025-06-27 10:46:51 +0100

    update condition

--- scripts/flexible-server/common-functions.sh
+++ scripts/flexible-server/common-functions.sh
@@ -31,10 +31,7 @@ function get_flexible_sql_servers() {
     resources
     | where type =~ 'microsoft.dbforpostgresql/flexibleservers'
     | where tags.autoShutdown == 'true'
-    | where not(tolower(tags.environment) == 'staging')
-    | where not(tolower(tags.environment) == 'development')
-    | where not(tolower(tags.environment) == 'demo')
-    | where not(tolower(tags.environment) == 'sandbox')
+    | where not(tolower(tags.environment) in~ ('staging', 'development', 'demo', 'sandbox'))
     $env_selector
     $area_selector
     $replica_selector
