9403b0ee7bf77da8a6388a661f3807971eebc98d
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2025-10-07 15:59:50 +0100

    Add auto shutdown for MySQL flexible servers

---
+++ scripts/mysql-flexible/common-functions.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+function get_mysql_servers() {
+  #MS az graph query to find and return a list of all MySQL Flexible Servers tagged to be included in the auto-shutdown process.
+  log "----------------------------------------------"
+  log "Running az graph query..."
+
+  if [ -z $1 ]; then
+    env_selector=""
+  elif [ $1 == "untagged" ]; then
+    env_selector="| where isnull(tags.environment)"
+  else
+    env_selector="| where tags.environment == '$1'"
+  fi
+
+  if [ -z $2 ]; then
+    area_selector=""
+  else
+    area_selector="| where tolower(tags.businessArea) == tolower('$2')"
+  fi
+
+  if [ -z $3 ]; then
+    replica_selector=""
+  elif [ $3 == "replica" ]; then
+    replica_selector="| where id contains 'replica'"
+  elif [ $3 == "primary" ]; then
+    replica_selector="| where id !contains 'replica'"
+  fi
+
+  az graph query -q "
+    resources
+    | where type =~ 'microsoft.dbformysql/flexibleservers'
+    | where tags.autoShutdown == 'true'
+    | where tags.startupMode != 'onDemand'
+    $env_selector
+    $area_selector
+    $replica_selector
+    | project name, resourceGroup, subscriptionId, ['tags'], properties.state, ['id']
+    " --first 1000 -o json
+
+  log "az graph query complete"
+}
+
+# Function that accepts the MySQL flexible server json as input and sets variables for later use to stop or start as required.
+function get_mysql_server_details() {
+  RESOURCE_GROUP=$(jq -r '.resourceGroup' <<< $mysqlserver)
+  SERVER_ID=$(jq -r '.id' <<< $mysqlserver)
+  SERVER_NAME=$(jq -r '.name' <<< $mysqlserver)
+  ENVIRONMENT=$(echo $SERVER_NAME | rev | cut -d'-' -f 1 | rev )
+  BUSINESS_AREA=$( jq -r 'if (.tags.businessArea | ascii_downcase) == "ss" then "cross-cutting" else .tags.businessArea | ascii_downcase end' <<< $mysqlserver)
+  STARTUP_MODE=$(jq -r '.tags.startupMode' <<< $mysqlserver)
+  SERVER_STATE=$(jq -r '.properties_state' <<< $mysqlserver)
+  SUBSCRIPTION=$(jq -r '.subscriptionId' <<< $mysqlserver)
+
+}
+
+function mysql_server_state_messages() {
+    ts_echo_color GREEN "About to run $MODE operation on mysql flexible server $SERVER_NAME Resource Group: $RESOURCE_GROUP"
+    ts_echo_color GREEN "Command to run: az mysql flexible-server $MODE --resource-group $RESOURCE_GROUP --name $SERVER_NAME --subscription $SUBSCRIPTION --no-wait || echo Ignoring any errors while $MODE operation on mysql flexible server"
+}
---
+++ scripts/mysql-flexible/mysql-flexible-status.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+# set -x
+shopt -s nocasematch
+
+# Source shared function scripts
+source scripts/mysql-flexible/common-functions.sh
+source scripts/common/common-functions.sh
+
+# Set variables for later use, MODE has a default but can be overridden at usage time
+# notificationSlackWebhook is used during the function call `auto_shutdown_notification`
+MODE=${1:-start}
+notificationSlackWebhook=$2
+SKIP="false"
+
+# Catch problems with MODE input, must be one of Start/Stop
+if [[ "$MODE" != "start" && "$MODE" != "stop" ]]; then
+    echo "Invalid MODE. Please use 'start' or 'stop'."
+    exit 1
+fi
+
+MYSQL_SERVERS=$(get_mysql_servers)
+mysql_server_count=$(jq -c -r '.count' <<< $MYSQL_SERVERS)
+log "$mysql_server_count MySQL Flexible Servers found"
+log "----------------------------------------------"
+
+# For each MySQL Flexible Server found in the function `get_mysql_servers` start another loop
+jq -c '.data[]' <<<$MYSQL_SERVERS | while read mysqlserver; do
+    # Function that returns the Resource Group, Id and Name of the MySQL Flexible Server and its current state as variables
+    get_mysql_server_details
+
+    # Set variables based on inputs which are used to decide when to SKIP an environment
+    if [[ $ENVIRONMENT == "stg" ]]; then
+        mysql_server_env=${ENVIRONMENT/stg/Staging}
+    elif [[ $ENVIRONMENT == "sbox" ]]; then
+        mysql_server_env=${ENVIRONMENT/sbox/Sandbox}
+    else
+        mysql_server_env=$ENVIRONMENT
+    fi
+
+    mysql_server_business_area=$BUSINESS_AREA
+
+    # SKIP variable updated based on the output of the `should_skip_start_stop` function which calculates its value
+    # based on the issues_list.json file which contains user requests to keep environments online after normal hours
+    SKIP=$(should_skip_start_stop $mysql_server_env $mysql_server_business_area $MODE "mysql-flexible")
+
+    # Setup message output templates for later use
+    logMessage="MySQL Flexible Server: $SERVER_NAME in Subscription: $SUBSCRIPTION  ResourceGroup: $RESOURCE_GROUP is in $SERVER_STATE state after $MODE action"
+    slackMessage="MySQL Flexible Server: *$SERVER_NAME* in Subscription: *$SUBSCRIPTION* is in *$SERVER_STATE* state after *$MODE* action"
+
+    # If SKIP is false then we progress with the status check for the particular MySQL flexible server in this loop run, if SKIP is true then do nothing
+    if [[ $SKIP == "false" ]]; then
+        log "$logMessage"
+        # Function call to send post shutdown status notification to Slack - Params: (Slack webhook, message template)
+        auto_shutdown_notification "$notificationSlackWebhook" "$slackMessage"
+    else
+        ts_echo_color AMBER "MySQL flexible server $SERVER_NAME (rg:$RESOURCE_GROUP) has been skipped from today's $MODE status notification"
+    fi
+done
